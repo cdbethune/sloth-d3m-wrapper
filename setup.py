454acd21e87076a8f904a647b2094ac743dd46b0
azunre <azunre@PaulLinuxGPUDev.jaavmej2qrwupdw4djg0r2u5ud.bx.internal.cloudapp.net>
2018-10-03 14:26:19 +0000

    updated hash to latest commit of sloth, 2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(name='SlothD3MWrapper',
     install_requires=["typing",
         "Sloth==2.0.0"],
     dependency_links=[
-        "git+https://github.com/NewKnowledge/sloth@bc1711a3e981b843a029b65db7da6169dab662d0#egg=Sloth-2.0.0"
+        "git+https://github.com/NewKnowledge/sloth@d38b8892fbefb4425d211d2cd858cfad91a2113e#egg=Sloth-2.0.0"
     ],
     entry_points = {
         'd3m.primitives': [
