b7cf4edc7fca8b51f4b95e72deb5d2b3e86aadaf
Jeffrey Gleason <jeffrey.gleason.3@gmail.com>
2019-01-17 17:02:31 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,8 +4,7 @@ setup(name='SlothD3MWrapper',
     version='2.0.1',
     description='A thin wrapper for interacting with New Knowledge time series tool library Sloth',
     packages=['SlothD3MWrapper'],
-    install_requires=["typing",
-        "Sloth==2.0.2"],
+    install_requires=["Sloth==2.0.2"],
     dependency_links=[
         "git+https://github.com/NewKnowledge/sloth@fafa4857f02c91c36c22e0d3efb16ab90d47e62d#egg=Sloth-2.0.2"
     ],
